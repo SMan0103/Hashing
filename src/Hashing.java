0db634eace54eae3f928dd178aedea0360261b90
Emil Thostrup Pedersen <steamman0103@gmail.com>
2022-09-08 15:14:36 +0200

    Hashing  og main virker for MD5 og SHA-512

---
+++ src/Hashing.java
@@ -0,0 +1,34 @@
+import java.math.BigInteger;
+import java.security.MessageDigest;
+
+public class Hashing {
+
+    public static String MD5Hash(String s) {
+        String result = "";
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update(s.getBytes());
+            result = new BigInteger(1, md.digest()).toString(16);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static String SHA512(String s) {
+        String result = "";
+
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-512");
+            md.update(s.getBytes());
+            result = new BigInteger(1, md.digest()).toString(16);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Hashing.MD5Hash("Hej med dig!"));
+        System.out.println(Hashing.SHA512("Hej med dig!"));
+    }
+}
